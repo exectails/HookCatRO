056377d78343e5305b4ec70c8bbecb0c7daa4768
Exec Tails <exec@dragonbyte.de>
2026-04-29 10:46:21 +0200

    Added minor connection patch for i20011214

--- HookCat/connect_address.cpp
+++ HookCat/connect_address.cpp
@@ -72,6 +72,17 @@ namespace HookCat
 				return;
 			}
 
+			// The Beta1 2001-12-14 client appears to be an English
+			// client, except that it uses the Japanese region setting,
+			// which changes the IP used. By forcing the English region,
+			// for the IP, we get past this.
+			auto beta1RegionAddress = Kitten.Find("RagExe.exe", "E8 36CBF6FF  A1 50055300  83E8 00  74 65  48");
+			if (beta1RegionAddress)
+			{
+				Kitten.Patch("RagExe.exe", beta1RegionAddress.value(), "E8 36CBF6FF  B8 01000000  83E8 00  74 65  48");
+				Kitten.Log.Info("  region patch applied at 0x%08X.", beta1RegionAddress.value());
+			}
+
 			if (enableArgs)
 			{
 				std::string cmdLine(GetCommandLineA());
@@ -134,17 +145,25 @@ namespace HookCat
 			Kitten.Log.Info("  new address: %s:%d", ip2, port);
 
 			// Alpha 2001-08-30
-			//0048BD56 | B9 40645200                | mov ecx,ragexe.526440                                |
-			//0048BD5B | C705 70605200 581B0000     | mov dword ptr ds:[526070],1B58                       | Port
-			//0048BD65 | E8 762C0100                | call <ragexe.sub_49E9E0>                             |
-			//0048BD6A | BF F8BA4E00                | mov edi,ragexe.4EBAF8                                | IP
-			//0048BD6F | 83C9 FF                    | or ecx,FFFFFFFF                                      |
+			// 0048BD56 | B9 40645200                | mov ecx,ragexe.526440                                |
+			// 0048BD5B | C705 70605200 581B0000     | mov dword ptr ds:[526070],1B58                       | Port
+			// 0048BD65 | E8 762C0100                | call <ragexe.sub_49E9E0>                             |
+			// 0048BD6A | BF F8BA4E00                | mov edi,ragexe.4EBAF8                                | IP
+			// 0048BD6F | 83C9 FF                    | or ecx,FFFFFFFF                                      |
+
+			// Beta1 2001-12-14
+			// 004AEEE5 | E8 36CBF6FF                | call ragexe.41BA20                                   |
+			// 004AEEEA | A1 50055300                | mov eax,dword ptr ds:[530550]                        | Get Region
+			// 004AEEEF | 83E8 00                    | sub eax,0                                            |
+			// 004AEEF2 | 74 65                      | je ragexe.4AEF59                                     |
+			// 004AEEF4 | 48                         | dec eax                                              |
+			// 004AEEF5 | 74 36                      | je ragexe.4AEF2D                                     |
 
 			// Beta1 2002-02-20
-			//004BB429 | BF DCA75400                | mov edi,ragexe.54A7DC                                | IP
-			//004BB42E | 83C9 FF                    | or ecx,FFFFFFFF                                      |
-			//004BB431 | 33C0                       | xor eax,eax                                          |
-			//004BB433 | C705 48A35900 F41A0000     | mov dword ptr ds:[59A348],1AF4                       | Port
+			// 004BB429 | BF DCA75400                | mov edi,ragexe.54A7DC                                | IP
+			// 004BB42E | 83C9 FF                    | or ecx,FFFFFFFF                                      |
+			// 004BB431 | 33C0                       | xor eax,eax                                          |
+			// 004BB433 | C705 48A35900 F41A0000     | mov dword ptr ds:[59A348],1AF4                       | Port
 		}
 	}
 }
